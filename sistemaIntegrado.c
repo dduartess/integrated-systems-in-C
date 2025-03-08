5ec2b215f0f484fa91f8df910303e84a501b6d9c
Duartess <dandandsoares@gmail.com>
2025-03-08 18:52:12 -0300

    add new options

--- sistemaIntegrado.c
+++ sistemaIntegrado.c
@@ -1,8 +1,15 @@
+
 #include <stdio.h>
 #include <locale.h>
 
+float convter(float amount, float price);
+
 int main(){
 
+    char resposta;
+
+do{
+    
     setlocale(LC_ALL, "Portuguese_Brazil");
     int resp;
 
@@ -34,7 +41,7 @@ int main(){
         else if (resp == 2){
             printf("\n Digite a temperatura em Farenheit: \n");
             scanf("%f", &F);
-            C = 5 / 9 * (F-32);
+            C = (5.0 / 9.0) * (F - 32);
             printf("\n A temperatura em Celsius é: %.1fºC", C);
         }
         else {
@@ -74,13 +81,125 @@ int main(){
             printf("Você está com Obesidade III\n");
         }
     }
+    else if(resp == 3){
+        int resp;
 
+        printf("---------------------------------------------------------------------------------------\n");
+        printf("Qual moeda você deseja converter? \n");
+        printf("---------------------------------------------------------------------------------------\n");
+        printf("\n1 - BRL ");
+        printf("\n2 - USD ");
+        printf("\n3 - EUR ");
+        printf("\n---------------------------------------------------------------------------------------\n");
+        scanf("%d", &resp);
+    
+            if(resp == 1){
+                int choice;
+                printf("\nVocê deseja converter de BRL para qual moeda: \n");
+                printf("---------------------------------------------------------------------------------------\n");
+                printf("\n1- USD");
+                printf("\n2- EUR\n");
+                printf("\n---------------------------------------------------------------------------------------\n");
+                    scanf("%d", &choice);
+                    if (choice == 1){
+                        float BRL, USD, price;
+                        printf("Digite o valor em BRL:  R$ ");
+                        scanf("%f", &BRL);
+                        printf("Digite o valor do USD: $");
+                        scanf("%f", &price);
+                        USD = convter(BRL, price);
+                        printf("\n---------------------------------------------------------------------------------------\n");
+                        printf("Você terá $%.2f USD", USD);
+                    }
+                    else if(choice == 2){
+                        float BRL, EUR, price;
+                        printf("Digite o valor em BRL:  R$ ");
+                        scanf("%f", &BRL);
+                        printf("Digite o valor do EUR: ?");
+                        scanf("%f", &price);
+                        EUR = convter(BRL, price);
+                        printf("\n---------------------------------------------------------------------------------------\n");
+                        printf("Você terá ?%.2f EUR", EUR);  
+                    }
+            }
+            else if(resp == 2){
+                int choice;
+                printf("\nVocê deseja converter de USD para qual moeda: \n");
+                printf("---------------------------------------------------------------------------------------\n");
+                printf("\n1- BRL");
+                printf("\n2- EUR");
+                printf("\n---------------------------------------------------------------------------------------\n");
+                scanf("%d", &choice);
+    
+                if (choice == 1){
+                    float USD, BRL, price;
+                    printf("\nDigite o valor em USD: $ ");
+                    scanf("%f", &USD);
+                    printf("\nDigite o valor do BRL: R$ ");
+                    scanf("%f", &price);
+                    BRL = convter(USD, price);
+                    printf("\n---------------------------------------------------------------------------------------\n");
+                    printf("Você terá R$%.2f BRL", BRL);                 
+                }
+                else if(choice == 2){
+                    float USD, EUR, price;
+                    printf("\nDigite o valor em USD: $ ");
+                    scanf("%f", &USD);
+                    printf("\nDigite o valor do EUR: ? ");
+                    scanf("%f", &price);
+                    EUR = convter(USD, price);
+                    printf("\n---------------------------------------------------------------------------------------\n");
+                    printf("Você terá ?%.2f EUR", EUR); 
+                }
+                
+            }
+            else if(resp == 3){
+                int choice;
+                printf("\nVocê deseja converter de EUR para qual moeda: \n");
+                printf("---------------------------------------------------------------------------------------\n");
+                printf("\n1- BRL");
+                printf("\n2- USD");
+                printf("\n---------------------------------------------------------------------------------------\n");
+                scanf("%d", &choice);
+                    if (choice == 1){
+                        float BRL, EUR, price;
+                        printf("\nDigite o valor em EUR: ? ");
+                        scanf("%f", &EUR);
+                        printf("\nDigite o valor do BRL: R$ ");
+                        scanf("%f", &price);
+                        BRL = convter(EUR, price);
+                        printf("\n---------------------------------------------------------------------------------------\n");
+                        printf("Você terá R$%.2f BRL", BRL); 
+                    }
+                    else if(choice == 2){
+                        float USD, EUR, price;
+                        printf("\nDigite o valor em EUR: ? ");
+                        scanf("%f", &EUR);
+                        printf("\nDigite o valor do USD: $ ");
+                        scanf("%f", &price);
+                        USD = convter(EUR, price);
+                        printf("\n---------------------------------------------------------------------------------------\n");
+                        printf("Você terá $%.2f USD", USD); 
+                    }
+            }
+            else{
+                printf("Erro! informe uma opção válida.");
+            }
+    }
     else{
         printf("Sistema indisponível no momento");
     }
 
+    printf("\nDeseja realizar uma nova consulta? (s/n) ");
+    scanf(" %c", &resposta);
+    
+} while (resposta == 's' || resposta == 'S');
 
 
+} 
 
-
+float convter(float amount, float price){
+float total;
+total = amount / price;
+return total;
 }
